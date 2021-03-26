dc68e3bc2ec71f574efd6dc3c0611d469bb197f5
Puti Megitasari <50572963+megitasariputi@users.noreply.github.com>
2021-03-26 16:53:57 +0700

    Update proses_delete_buku_tamu.php

--- proses_delete_buku_tamu.php
+++ proses_delete_buku_tamu.php
@@ -1,6 +1,8 @@
 <?php 
     require_once 'mysql_koneksi.php';
 
+    session_start();
+
     // perintah SQL (4)
     $id = $_GET['idtamu'];
 
@@ -9,12 +11,16 @@
 
     // Eksekusi Perintah menerapkan ke tabel
     if($conn->query($sql)===true){
+        $_SESSION['update_status'] = 1;
+        $_SESSION['update_message'] = '<strong>Berhasil!</strong>Data berhasil terhapus';
         // header('location:halaman_buku_tamu.php');
         echo "<script>alert('Berhasil Terhapus');
         location.assign('halaman_buku_tamu.php');
         </script>";
     }
     else {
+        $_SESSION['update_status'] = 0;
+        $_SESSION['update_message'] = '<strong>Gagal!</strong>Data gagal terhapus';
         echo "<script>alert('Gagal Terhapus');
         location.assign('halaman_buku_tamu.php');
         </script>";
